49d6746920d9327565874653b19ad7b030d6928a
Jhustie Cruz <150127507+jastieee@users.noreply.github.com>
2024-08-12 12:07:06 +0800

    Update about.js

--- about.js
+++ about.js
@@ -10,7 +10,7 @@ function calculateAge() {
     const ageInMilliseconds = now - birthDate;
     const ageInYears = ageInMilliseconds / (1000 * 60 * 60 * 24 * 365.25);
 
-    return ageInYears.toFixed(15);
+    return ageInYears.toFixed(12);
 }
 
 function updateAge() {
